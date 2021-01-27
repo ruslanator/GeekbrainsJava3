898f6fb4bc58c168a1a9c686a840163abcdc14f5
ruslanator <latyshevruslan@ya.ru>
2021-01-27 22:26:21 +0300

    Lesson7

---
+++ Test.java
@@ -0,0 +1,12 @@
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(METHOD)
+public @interface Test {
+    int priority() default 3;
+}
